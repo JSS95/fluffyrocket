176435d52d6690a860b9d872c39408d3d05cdae4
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-12-01 15:47:00 +0900

    feat: implement FluffyRocket
    Modify class inheritance structure

--- src/fluffyrocket/base.py
+++ src/fluffyrocket/base.py
@@ -5,6 +5,7 @@ from itertools import combinations
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from torch.nn import Module
 
 from ._minirocket import fit as _fit
@@ -20,6 +21,8 @@ __all__ = [
 
 class MiniRocketBase(Module, abc.ABC):
     """Base class for MiniRocket-like transformers.
+
+    Subclass should implement :meth:`ppv`.
 
     Parameters
     ----------
@@ -78,6 +81,52 @@ class MiniRocketBase(Module, abc.ABC):
 
         return self
 
-    @abc.abstractmethod
     def forward(self, x):
+        _, num_channels, _ = x.shape
+
+        features = []
+        feature_index_start = 0
+        combination_index = 0
+        num_channels_start = 0
+
+        for i in range(len(self.dilations)):
+            dilation = self.dilations[i].item()
+            padding = ((9 - 1) * dilation) // 2
+            num_features_this_dilation = self.num_features_per_dilation[i].item()
+
+            C = F.conv1d(
+                x, self.kernels, padding=padding, dilation=dilation, groups=num_channels
+            )
+            C = C.view(-1, num_channels, 84, C.shape[-1])
+
+            for j in range(84):
+                feature_index_end = feature_index_start + num_features_this_dilation
+                num_channels_this_combination = self.num_channels_per_combination[
+                    combination_index
+                ].item()
+                num_channels_end = num_channels_start + num_channels_this_combination
+                channels_this_combination = self.channel_indices[
+                    num_channels_start:num_channels_end
+                ]
+
+                C_sum = torch.sum(C[:, channels_this_combination, j, :], dim=1)
+
+                biases_this_kernel = self.biases[feature_index_start:feature_index_end]
+
+                if (i + j) % 2 == 0:
+                    ppv = self.ppv(C_sum.unsqueeze(-1), biases_this_kernel)
+                else:
+                    ppv = self.ppv(
+                        C_sum[:, padding:-padding].unsqueeze(-1), biases_this_kernel
+                    )
+                features.append(ppv)
+
+                feature_index_start = feature_index_end
+                combination_index += 1
+                num_channels_start = num_channels_end
+
+        return torch.cat(features, dim=1)
+
+    @abc.abstractmethod
+    def ppv(self, x, biases):
         raise NotImplementedError
